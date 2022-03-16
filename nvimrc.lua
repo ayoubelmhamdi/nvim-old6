a61e74dd2c83eae767d294f813af02cc9f032340
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-03-16 20:01:02 +0100

    update backling indent to use block

--- nvimrc.lua
+++ nvimrc.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: unused-local
 vim.g.mapleader = " "
+vim.opt.termguicolors = true
 local cmd = vim.cmd
-cmd "set termguicolors"
 
 local tsp_ok, tsp = pcall(require, "tsp")
 if not tsp_ok then
@@ -41,16 +41,31 @@ nvim_treesitter_configs.setup {
 -- theme
 
 --Map blankline
-vim.g.indent_blankline_char = "┊"
-vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_char_highlight = "LineNr"
-vim.g.indent_blankline_show_trailing_blankline_indent = false
+-- vim.g.indent_blankline_char = "┊"
+-- vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
+-- vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+-- vim.g.indent_blankline_char_highlight = "LineNr"
+-- vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+
+require("indent_blankline").setup {
+    char = "",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    show_trailing_blankline_indent = false,
+}
 
 -- theme
 vim.o.background = "dark"
 
-cmd [[
+vim.cmd [[
 " transparent backgroub
 colorscheme gruvbox
 hi Normal guibg=NONE ctermbg=NONE
@@ -74,7 +89,7 @@ augroup end
 ]]
 
 -- view cursor yank
-cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"  -- disabled in visual mode
+vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"  -- disabled in visual mode
 
 ----require "ay_cmp"
 ----require "ay_luasnip"
@@ -88,9 +103,9 @@ require "ls.ay_efm"
 require("flutter-tools").setup {}
 
 --
-cmd "source ~/.config/nvim/lua/vimScript/init.vim"
+vim.cmd "source ~/.config/nvim/lua/vimScript/init.vim"
 
-cmd "au BufRead,BufNewFile *.conf		setfiletype bash"
-cmd "au BufRead,BufNewFile *.fish		setfiletype fish"
-cmd "let g:do_filetype_lua = 1"
--- cmd "let g:did_load_filetypes = 1"
+vim.cmd "au BufRead,BufNewFile *.conf		setfiletype bash"
+vim.cmd "au BufRead,BufNewFile *.fish		setfiletype fish"
+vim.cmd "let g:do_filetype_lua = 1"
+-- vim.cmd "let g:did_load_filetypes = 1"
