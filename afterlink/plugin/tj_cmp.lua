107805931d5fbf91de3ddafaae58c376177c3d69
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-28 22:14:53 +0100

    add <c-n> <c-p>

--- afterlink/plugin/tj_cmp.lua
+++ afterlink/plugin/tj_cmp.lua
@@ -33,7 +33,10 @@ cmp.setup({
     },
 
   mapping = {
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-u>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.close(),
     ["<c-y>"] = cmp.mapping(
