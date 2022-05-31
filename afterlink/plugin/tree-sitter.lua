9e4912b292766efe520c731e61894cdb352d3425
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-31 23:43:40 +0100

    up

--- afterlink/plugin/tree-sitter.lua
+++ afterlink/plugin/tree-sitter.lua
@@ -1,7 +1,12 @@
+-- if true then
+--     return
+-- end
 local nvim_treesitter_configs = Prequire "nvim-treesitter.configs"
 
 nvim_treesitter_configs.setup {
-   -- ensure_installed = "maintained",
+    -- ensure_installed = "maintained",
+    -- ensure_installed = { "c", "lua", "rust" },
+    disable = { "dart" },
     sync_install = false,
     highlight = { enable = true, additional_vim_regex_highlighting = false },
 }
